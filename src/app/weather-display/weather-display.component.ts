b264fa27607c1e442640b2ff90403c81c8e754a9
AllenChang <s91503486f@gmail.com>
2021-08-19 02:48:59 +0800

    show weather data

--- src/app/weather-display/weather-display.component.ts
+++ src/app/weather-display/weather-display.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-weather-display',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./weather-display.component.scss']
 })
 export class WeatherDisplayComponent implements OnInit {
+
+  @Input() weatherData;
 
   constructor() { }
 
