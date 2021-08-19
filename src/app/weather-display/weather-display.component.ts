56b347721794ca3d913322494fc7736c1b66bd4d
AllenChang <s91503486f@gmail.com>
2021-08-20 03:24:56 +0800

    add bar-chart to display component

--- src/app/weather-display/weather-display.component.ts
+++ src/app/weather-display/weather-display.component.ts
@@ -6,12 +6,40 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./weather-display.component.scss']
 })
 export class WeatherDisplayComponent implements OnInit {
+  public barChartDisplayData;
+  
+  private _weatherData: any;
+  @Input() set weatherData(value: any) {
+    this._weatherData = value;
+    this.toBarChartDisplayData();
+  }
 
-  @Input() weatherData;
+  get weatherData(): any {
+
+    return this._weatherData;
+
+  }
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  toBarChartDisplayData(){
+    let barChartData = [];
+    let weathers = this._weatherData.consolidated_weather.slice(0, this._weatherData.consolidated_weather.length-1);
+    
+    weathers.forEach(weather => {
+      barChartData.push(weather.min_temp);
+      barChartData.push(weather.max_temp);
+      barChartData.push(0);
+    });
+
+    barChartData.pop();
+
+    let dates = weathers.map(weather => weather.applicable_date);
+
+    this.barChartDisplayData = { data: barChartData, date: dates};
   }
 
 }
